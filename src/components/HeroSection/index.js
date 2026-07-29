8a8a049e1b9213c6492f6f38ccf1562d77910f0f
FelipeBertol <135773873+FelipeBertol@users.noreply.github.com>
2026-07-28 22:56:25 -0300

    Ajuste de AND para &

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -4,7 +4,7 @@ const HeroSection = () => {
     return (
         <section className='hero-section' id="start"
         >
-           <h1>JAIANNY <small>and</small> FELIPE</h1>
+           <h1>JAIANNY <small>&</small> FELIPE</h1>
            <h2>11.10.2026</h2>
         </section>
     );
