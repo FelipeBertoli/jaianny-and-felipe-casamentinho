e8732cc53f0de124b62a760fff5a6d969df0012f
FelipeBertol <135773873+FelipeBertol@users.noreply.github.com>
2026-07-28 22:54:02 -0300

    Versão inicial para o convite

--- src/sections/StartSection.js
+++ src/sections/StartSection.js
@@ -10,12 +10,12 @@ export default function DaySection() {
             <div className="start-content">
                 <Countdown />
                 <YouTubeEmbed />
-                <div className="day-detail">
+                {/* <div className="day-detail">
                     <h3>Save the Date</h3>
                     <h4>Outubro de 2026</h4>
                     <Calendario />
-                    {/* <span className="day-time">Às 16:00 horas</span> */}
-                </div>
+                    {/* <span className="day-time">Às 16:00 horas</span> 
+                </div> */}
                 <div className="day-detail">
                       <h3>Local do Evento</h3> 
                         <text><b>Espaço Floratta</b> • Av. José Bonifácio • N° 2029 • Cambé, PR • 86181-570</text>
